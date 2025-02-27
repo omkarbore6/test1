118acd86e0b0dcf1d98c023e89b7763abbc409e8
omkarbore6 <133559436+omkarbore6@users.noreply.github.com>
2025-02-27 09:01:57 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,7 +18,7 @@ function App() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon text-white"></span>
           </button>
           <div
             className="collapse navbar-collapse text-white "
